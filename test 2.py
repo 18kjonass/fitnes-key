ae10a2e008a7c056af9721d48b69a9c10382524e
18kjonass <112894153+18kjonass@users.noreply.github.com>
2024-02-29 13:22:03 +0000

    Add files via upload

--- test 2.py
+++ test 2.py
@@ -3,6 +3,7 @@ import firebase_admin
 import serial
 from firebase_admin import credentials
 from firebase_admin import db
+import matplotlib.pyplot as plt
 
 cred = credentials.Certificate("C:/Users/18KJonass.ACC/Downloads/fitnes-key-main/fitnes-key-main/computer-science-fitnes-firebase-adminsdk-lm0a4-65b841b747.json")
 firebase_admin.initialize_app(cred,{'databaseURL': 'https://computer-science-fitnes-default-rtdb.europe-west1.firebasedatabase.app/'})
@@ -14,5 +15,18 @@ ref = ref.child('users/'+ user_name)
 result = ref.get()
 print(type(result))
 
+x = []
+y = []
 for key, value in result.items():
-    print(value)
+   #print(value)
+    x.append(key)
+    y.append(value['distance'])
+    
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+
+
+plt.show()
