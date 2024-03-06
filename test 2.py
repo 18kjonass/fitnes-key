ada61fcd6a6ef3ed412267a8393787391004adab
18kjonass <112894153+18kjonass@users.noreply.github.com>
2024-03-06 13:11:42 +0000

    Add files via upload

--- test 2.py
+++ test 2.py
@@ -29,19 +29,26 @@ y3 = []
 u need now to set goals for yours asking steps and if they need more or less
 '''
 
+avgsteps = 0
+avgdistance = 0
+count = 0
 
 for key, value in result.items():
-   #print(value)
     timestamp_string = key
     dt_obj = datetime.fromtimestamp(int(timestamp_string))
     print("date_time:",dt_obj.strftime('%m-%d'))
     
-    x1.append(dt_obj.strftime('%m-%d'))
-    y1.append(int(value['steps']))
-    
-    plt.plot(x1,y1)
-    plt.title("Steps")
-    
+    if int(value['steps']) > 0 :
+        x1.append(dt_obj.strftime('%m-%d'))
+        y1.append(int(value['steps']))
+        
+        avgsteps = avgsteps + int(value['steps']) 
+        count += 1
+
+avgsteps = avgsteps/count   
+        
+plt.plot(x1,y1)
+plt.title("Steps " + str(round(avgsteps)))   
 plt.suptitle("Step Counter")
 plt.show()
     
@@ -52,19 +59,27 @@ for key, value in result.items():
         timestamp_string = key
         dt_obj = datetime.fromtimestamp(int(timestamp_string))
         print("date_time:",dt_obj.strftime('%m-%d'))
+        
+        if int(value['distance']) > 0 :
+            x2.append(dt_obj.strftime('%m-%d'))
+            y2.append(int(value['distance']))
+                
+            avgdistance = avgdistance + int(value['distance'])
             
-        x2.append(dt_obj.strftime('%m-%d'))
-        y2.append(int(value['distance']))
-            
+        print(avgdistance)
+        print(count)
+        #avgdistance = round(((avgdistance/count)/100), 2)
+        
         plt.plot(x2,y2)
-        plt.title("Distance")
+        plt.title("Distance " + str(avgdistance/count))
     elif user_input == 2:
         timestamp_string = key
         dt_obj = datetime.fromtimestamp(int(timestamp_string))
         print("date_time:",dt_obj)
-            
-        x3.append(str(dt_obj.time()))
-        y3.append(int(value['steps']))
+         
+        if int(value['steps']) > 0 :
+            x3.append(str(dt_obj.time()))
+            y3.append(int(value['steps']))
             
         plt.plot(x3,y3)
         plt.title("Time")
@@ -72,19 +87,24 @@ for key, value in result.items():
         timestamp_string = key
         dt_obj = datetime.fromtimestamp(int(timestamp_string))
         print("date_time:",dt_obj.strftime('%m-%d'))
+        
+        if int(value['distance']) > 0 :
+            x2.append(dt_obj.strftime('%m-%d'))
+            y2.append(int(value['distance']))
             
-        x2.append(dt_obj.strftime('%m-%d'))
-        y2.append(int(value['distance']))
-            
+            avgdistance = avgdistance + int(value['distance']) 
+        
+        avgdistance = avgdistance/count
         plt.subplot(2, 1, 1)
         plt.plot(x2,y2)
-        plt.title("Distance")
+        plt.title("Distance " + str(avgdistance))
         
         dt_obj = datetime.fromtimestamp(int(timestamp_string))
         print("date_time:",dt_obj)
-            
-        x3.append(str(dt_obj.time()))
-        y3.append(int(value['steps']))
+         
+        if int(value['steps']) > 0 :
+            x3.append(str(dt_obj.time()))
+            y3.append(int(value['steps']))
             
         plt.subplot(2, 1, 2)
         plt.plot(x3,y3)
